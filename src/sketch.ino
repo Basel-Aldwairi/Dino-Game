5d58365402cb5d00a89c6ea4be11097058d97b37
Basel-AlDwairi <aldwairi.basel0@gmail.com>
2026-03-07 19:25:22 +0300

    full game, toggle and graphics

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,155 +1,245 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 
-
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 const int read_button = 8;
 
-int game_array[2][16] = {0}; 
+int game_array[2][16] = {0};
+int player_state = 1;
+bool game_on = true;
 
 int rows = 2, cols = 16;
 
-int max_jump_time = 500;
-int time_last_jump = 0;
-int jump_delay = 500;
-int time_last_fall = 0;
+int max_jump_time = 600;
+long time_last_jump = 0;
+int jump_delay = 300;
+long time_last_fall = 0;
 
-int min_time_between_obstacle = 3000; 
-int time_between_obstacle = 0;
-int time_between_scenery_move = 500;
-int time_scenery_moved = 0;
+int min_time_between_obstacle = 500;
+long time_between_obstacle = 0;
+int time_between_scenery_move = 200;
+long time_scenery_moved = 0;
+int previous_obstacle = 0;
 
-void print_array(){
+int min_time_between_lane_toggle = 100;
+long last_lane_toggle_time = 0;
 
-  for(int i = 0; i < rows; i++){
-    for(int j = 0; j < cols; j++){
+long game_time_start = millis();
+
+void print_array()
+{
+
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < cols; j++)
+    {
       Serial.print(game_array[i][j]);
     }
     Serial.println();
   }
-
-
 }
 
-bool can_jump(){
+bool can_jump()
+{
   int ground_time = millis() - time_last_fall;
-  if (game_array[1][0] == 1 && ground_time >= jump_delay) return true;
-  else return false;
+  if (player_state == 1 && ground_time >= jump_delay)
+    return true;
+  else
+    return false;
 }
 
-bool jump(){
+bool jump()
+{
 
-  if(can_jump()){
-    game_array[0][0] = 1;
-    game_array[1][0] = 0;
+  if (can_jump())
+  {
+    player_state = 0;
 
     time_last_jump = millis();
-    
+
     return true;
   }
-  else return false;
-  
+  else
+    return false;
 }
 
-bool can_fall(){
+bool can_fall()
+{
   int air_time = millis() - time_last_jump;
-  if (game_array[0][0] == 1 && air_time >= max_jump_time) return true;
-  else return false;
+  if (player_state == 0 && air_time >= max_jump_time)
+    return true;
+  else
+    return false;
 }
 
-bool fall(){
+bool fall()
+{
 
-  if(can_fall()){
-    game_array[0][0] = 0;
-    game_array[1][0] = 1;
-  
+  if (can_fall())
+  {
+    player_state = 1;
+
     time_last_fall = millis();
 
     return true;
   }
-  else return false;
-
+  else
+    return false;
 }
 
-void move_scenery(){
+void toggle_lane()
+{
+  int since_last_lane_toggle = millis() - last_lane_toggle_time;
+
+  if (since_last_lane_toggle < min_time_between_lane_toggle) return;
+
+  if (player_state == 0)
+    player_state = 1;
+  else
+    player_state = 0;
+  
+    last_lane_toggle_time = millis();
+}
+
+void put_player_in_game()
+{
+  lcd.setCursor(0, player_state);
+  lcd.print(1);
+}
+
+void put_player_in_game_graphics()
+{
+  lcd.setCursor(0, player_state);
+  lcd.print(">");
+}
+
+void move_scenery()
+{
 
   int time_last_scenery = millis() - time_scenery_moved;
-  
-  if (time_last_scenery < time_between_scenery_move) return;
 
-  for(int i = 0; i < rows; i++){
-    for(int j = 1; j < cols; j++){
+  if (time_last_scenery < time_between_scenery_move)
+    return;
 
-      game_array[i][j- 1] = game_array[i][j];
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 1; j < cols; j++)
+    {
+
+      game_array[i][j - 1] = game_array[i][j];
       game_array[i][j] = 0;
     }
-  } 
+  }
   time_scenery_moved = millis();
-
+  add_obstacle();
 }
 
-int create_obstacle(){
+int create_obstacle()
+{
 
   int time_last_obstacle = millis() - time_between_obstacle;
 
-  if (time_last_obstacle < min_time_between_obstacle){
-    // Serial.print("Failed\n");
-    return 0;
-  }
+  // if (time_last_obstacle < min_time_between_obstacle)
+  // {
+  //   // Serial.print("Failed\n");
+  //   return 0;
+  // }
 
-  float fail_chance = 0.995;
-  int rolls = 2 + (int)(1/ fail_chance);
+  int fail_in_rolls = 6;
+  int rolls = 2 + fail_in_rolls;
   int obstecle = random(rolls);
   // Serial.print(obstecle);
   time_between_obstacle = millis();
 
-  if (obstecle < rolls - 2){
+  if (obstecle < rolls - 2)
+  {
     return 0;
-  } 
+  }
 
-  if (obstecle == rolls - 2){
+  if (obstecle == rolls - 2)
+  {
     return 1;
   }
 
-  if (obstecle == rolls - 1){
+  if (obstecle == rolls - 1)
+  {
     return 2;
   }
-
 }
 
-void add_obstacle(){
+void add_obstacle()
+{
   int obstacle = create_obstacle();
 
-  if (obstacle == 0){
+  if (obstacle == 0)
+  {
+    previous_obstacle = 0;
     return;
   }
 
-  if (obstacle == 1){
-    game_array[1][cols- 1] = 2;
+  if (obstacle == 1 || obstacle == 2 && previous_obstacle == 1)
+  {
+    game_array[1][cols - 1] = 2;
+    previous_obstacle = 1;
+    return;
   }
 
-  if (obstacle == 2){
-    game_array[0][cols- 1] = 2;
+  if (obstacle == 2 || obstacle == 1 && previous_obstacle == 2)
+  {
+    game_array[0][cols - 1] = 2;
+    previous_obstacle = 2;
+    return;
   }
 }
 
+bool is_collision()
+{
+  if (game_array[player_state][0] == 2)
+    return true;
 
-
-bool collision(){
+  else
+    return false;
 }
 
-int jump_time(){
+void lose_screen()
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Lose Play again?");
+  lcd.setCursor(0, 1);
+  lcd.print((millis() - game_time_start) / 1000);
+  lcd.print(" s");
+}
 
+void collision_logic()
+{
+  if (is_collision())
+  {
+    lose_screen();
+    game_on = false;
+  }
+  else
+  {
+    lcd.clear();
+    print_lcd_graphics();
+    put_player_in_game_graphics();
+  }
+}
+
+int jump_time()
+{
   int time_jump_elapsed = millis() - time_last_jump;
-
 }
 
-void print_lcd(){
+void print_lcd()
+{
 
-  for(int i = 0; i < rows; i++){
-    for(int j = 0; j < cols; j++){
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < cols; j++)
+    {
       lcd.setCursor(j, i);
       lcd.print(game_array[i][j]);
     }
@@ -157,9 +247,44 @@ void print_lcd(){
   }
 }
 
+void print_lcd_graphics()
+{
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < cols; j++)
+    {
+      lcd.setCursor(j, i);
+      int game_object = game_array[i][j];
 
+      switch (game_object)
+      {
+      case 0:
+        break;
+      case 1:
+        lcd.print(">");
+        break;
+      case 2:
+        lcd.print("-");
+        break;
+      }
+    }
+    // Serial.println();
+  }
+}
 
-void setup() {
+void clear_game_array()
+{
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < cols; j++)
+    {
+      game_array[i][j] = 0;
+    }
+  }
+}
+
+void setup()
+{
 
   lcd.begin(16, 2);
   // lcd.print("This a __");
@@ -169,29 +294,44 @@ void setup() {
 
   // print_array();
   game_array[1][0] = 1;
-
 }
 
-void loop() {
-
-  // lcd.setCursor(0, 1);
-  // lcd.print(millis() / 1000);
-
+void loop()
+{
   int buttonState = digitalRead(read_button);
-  if(buttonState == HIGH){
-    jump();
+  if (game_on)
+  {
+
+    // lcd.setCursor(0, 1);
+    // lcd.print(millis() / 1000);
+
+    if (buttonState == HIGH)
+    {
+      // jump();
+      toggle_lane();
+    }
+    // fall();
+
+    // add_obstacle();
+    move_scenery();
+    collision_logic();
+
+    // Serial.println("\n\n\n\n\n\n\n\n\n\n");
+    // print_array();
+    // print_lcd();
+
+    delay(50);
   }
-  fall();
+  else
+  {
+    if (buttonState == HIGH)
+    {
+      game_on = true;
+      clear_game_array();
 
-  add_obstacle();
-  move_scenery();
-
-
-  // Serial.println("\n\n\n\n\n\n\n\n\n\n");
-  // print_array();
-  print_lcd();
-  
-
-  delay(50);
-
+      delay(200);
+      game_time_start = millis();
+      Serial.println(game_time_start / 1000);
+    }
+  }
 }
